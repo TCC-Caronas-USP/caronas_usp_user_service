cf5cc0bc2dcd9c26be853b223d46b027bc22c1a2
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-10-11 13:18:39 -0300

    Adiciona autenticação com Firebase

---
+++ backend/user_service/authentication.py
@@ -0,0 +1,32 @@
+from firebase_admin import auth, credentials, initialize_app
+from rest_framework import authentication
+
+from .exceptions import FirebaseError, InvalidAuthToken, NoAuthToken
+
+cred = credentials.Certificate('firebase-credentials.json')
+
+default_app = initialize_app(cred)
+
+
+class FirebaseAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        auth_header = request.META.get("HTTP_AUTHORIZATION")
+        if not auth_header:
+            raise NoAuthToken("No auth token provided")
+
+        id_token = auth_header.split(" ").pop()
+        decoded_token = None
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+        except Exception:
+            raise InvalidAuthToken("Invalid auth token")
+
+        if not id_token or not decoded_token:
+            return None
+
+        try:
+            uid = decoded_token.get("uid")
+        except Exception:
+            raise FirebaseError()
+
+        return (None, uid)
