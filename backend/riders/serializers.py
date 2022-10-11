8d85d18b1164150864d7c09e5cf58195a7376227
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-10-11 13:21:03 -0300

    Melhora serializers

--- backend/riders/serializers.py
+++ backend/riders/serializers.py
@@ -2,19 +2,12 @@ from rest_framework import serializers
 from .models import Rider, Vehicle
 
 
-class RiderSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Rider
-        fields = '__all__'
-
-
 class RiderUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rider
         fields = '__all__'
-        read_only_fields = ['email']
+        read_only_fields = ['email', 'name', 'vehicles']
 
 
 class VehicleSerializer(serializers.ModelSerializer):
@@ -22,3 +15,14 @@ class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
         fields = '__all__'
+
+
+class RiderSerializer(serializers.ModelSerializer):
+    vehicles = VehicleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Rider
+        fields = '__all__'
+        extra_kwargs = {
+            'uid': {'write_only': True}
+        }
