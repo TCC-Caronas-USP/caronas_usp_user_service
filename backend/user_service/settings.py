97adc9bcc6046941dd8ae29c870020c07743e6f6
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-11-20 19:55:49 -0300

    Corrige debug

--- backend/user_service/settings.py
+++ backend/user_service/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)#=)5ip%jj2jtfsh^%7wppipncnokof)jer*z*udue&h)36k*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
