4580ab7e254bc45e76145aaf3cce6573b5496fb9
Gabriel Desiderá <gabriel.desidera@inspira.com.br>
2022-11-21 00:17:07 -0300

    Coloca settings em env var

--- backend/user_service/settings.py
+++ backend/user_service/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#=)5ip%jj2jtfsh^%7wppipncnokof)jer*z*udue&h)36k*j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     'localhost',
